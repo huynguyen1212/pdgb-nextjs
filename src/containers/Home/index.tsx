dd821185caa7fa6498da723e765cedf2772b316e
HuyNQ <huynq@HuyNQ-FADERLESS-MAC.local>
2023-11-27 09:57:10 +0700

    update home

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -3,17 +3,35 @@ import Banner from "./Banner";
 import FormRequestCreateClub from "./FormRequestCreateClub";
 import { useSession } from "next-auth/react";
 import ListClubs from "./ListClubs";
+import { useQuery } from "react-query";
+import { requestToken } from "src/api/axios";
+import API_URL from "src/api/url";
 
 export default function Home() {
+  // api
   const { data: section }: any = useSession();
+
+  const { data: check } = useQuery(["CHECK_IS_IN_CLUB", section], async () => {
+    const response = await requestToken({
+      method: "GET",
+      url: API_URL.CLUBS.CHECK_IS_IN_CLUB,
+    });
+    return response?.status === 200 ? true : false;
+  });
 
   return (
     <SHome>
       <Banner />
 
-      {section && <FormRequestCreateClub />}
+      {check !== false ? (
+        <></>
+      ) : (
+        <>
+          <FormRequestCreateClub />
 
-      <ListClubs />
+          <ListClubs />
+        </>
+      )}
     </SHome>
   );
 }
